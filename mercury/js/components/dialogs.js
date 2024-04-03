9f23b56c0ae3ecc00cfefcce9188567c70d429a0
ergesovd380 <ergesovd380@gmail.com>
2024-04-03 16:13:58 +0300

    Add files

--- mercury/js/components/dialogs.js
+++ mercury/js/components/dialogs.js
@@ -19,3 +19,51 @@ showBtnDialogChange.addEventListener("click", () => {
 closeBtnDialogChange.addEventListener("click", () => {
   dialogChange.close();
 });
+
+
+// Dialog-project
+const fileName = document.querySelector('.dialog-proj_form-file-name');
+window.addEventListener('DOMContentLoaded', () => {
+  fileName.classList.add('d-none');
+});
+function updateFileProj() {
+  const fileNameText = document.querySelector('.dialog-proj_form-file-name-text');
+  if (document.getElementById('import-proj-file').files.length > 0) {
+    fileName.classList.remove('d-none');
+    fileNameText.innerHTML = document.getElementById('import-proj-file').files[0].name;
+  };
+};
+function deleteFileProj() {
+  const fileNameText = document.querySelector('.dialog-proj_form-file-name-text');
+  if (document.getElementById('import-proj-file').files.length > 0) {
+    console.log('Start')
+    fileName.classList.add('d-none');
+    fileNameText.innerHTML = '';
+    const files = document.getElementById('import-proj-file').files;
+    Object.keys(files).forEach(key => delete items[key]);
+  };
+};
+
+
+// Dialog analogue
+const fileNameAnalog = document.querySelector('.dialog_analog_form-file-name');
+window.addEventListener('DOMContentLoaded', () => {
+  fileNameAnalog.classList.add('d-none');
+});
+function updateFileAnalog() {
+  const fileNameText = document.querySelector('.dialog_analog_form-file-name-text');
+  if (document.getElementById('import-analog-file').files.length > 0) {
+    fileNameAnalog.classList.remove('d-none');
+    fileNameText.innerHTML = document.getElementById('import-analog-file').files[0].name;
+  };
+};
+function deleteFileAnalog() {
+  const fileNameText = document.querySelector('.dialog-analog_form-file-name-text');
+  if (document.getElementById('import-analog-file').files.length > 0) {
+    console.log('Start')
+    fileNameAnalog.classList.add('d-none');
+    fileNameText.innerHTML = '';
+    const files = document.getElementById('import-analog-file').files;
+    Object.keys(files).forEach(key => delete items[key]);
+  };
+};
